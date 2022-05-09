d26b7208700da0e4486f376d9b095999c2a929a1
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-09 09:29:54 +0300

    Да се дефинират: класовете: 	Клас филм (Film), който да притежава: Име на филма; Година на излизане; Жанр; Цена. 	 Клас билет (Ticket), който да притежава: Номер на ред; Място в реда; Дата на прожекция; Филм ( указател към обект от клас Film); Цена. Да се дефинират класове – наследници на Ticket: 	 Студентски билет 	Групов билет - съдържа информация за брой посетители, не повече от 25; Да се напише метод, който реализира продажба на билет: 	цената за студентски билет е 20% по-ниска от редовната. 	цената за групов билет се изчислява според броя посетители – над 10 ползват 10% отстъпка, над 20 ползват 20% отстъпка. Ред и място са пожелание на посетителя (груповия билет се посочва първото място). Да се дефинира клас Киносалон, който да притежава: 	Масив от места (двумерен масив) с 10 реда и 16 колони; 	Брой продадени билети; Да се напише метод, който запазва местата на продадените билети. Запазването да става последователно от началото (ред1, място1).

---
+++ Cinema/Cinema/Cinema.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Film.h"
+#include "GroupTicket.h"
+#include "MovieTheater.h"
+#include "StudentTicket.h"
+#include "Ticket.h"
+
+int main()
+{
+	MovieTheater cinema;
+	Ticket* array[5];
+	Film film("John Wick 3", "15/03/2019", "Action", 20);
+	Ticket ticket(0, 3,"15/03/2019", 20, film);
+	cinema.AddNewPeople(ticket.GetSeatNum(), ticket.GetRowNum(), 1);
+	StudentTicket studentTicket(2, 3, "15/03/2019", 20, film);
+	cinema.AddNewPeople(studentTicket.GetSeatNum(), studentTicket.GetRowNum(), 1);
+	GroupTicket groupTicket(22, 2, 8, "15/03/2019", 20, film);
+
+	array[0] = &ticket;
+	array[1] = &studentTicket;
+	array[2] = &groupTicket;
+
+	for (int i = 0; i < 3; i++)
+	{
+		array[i]->BuyTicket(i, 1);
+	}
+
+	cinema.AddNewPeople(groupTicket.GetSeatNum(), groupTicket.GetRowNum(),22);
+	ticket.Print();
+	studentTicket.Print();
+	groupTicket.Print();
+	cinema.PrintAllSeats();
+}
+
